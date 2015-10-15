52f2abdb80f03f8d60502924b45c8e07dd788a2d
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2015-10-16 00:25:08 +0100

    Working algebra translation

--- algebraToSQL.sql
+++ algebraToSQL.sql
@@ -6,7 +6,12 @@ select mail
 from Loga natural join Login
 where sucesso='nao';
 
-select data_nasc
-from Pessoas natural join Contem_Registos natural join Editam
-where nome_pagina='facebook' and nome_registo='facebook';
+select * from Pessoas natural join (Editam natural join Contem_Registos) where nome_registo='facebook' or nome_pagina='facebook';
 
+select data_nasc
+from Pessoas natural join (
+select * from Editam natural join(
+select mail, nome_registo from Contem_Registos natural join Editam as P2 where nome_registo='facebook') where nome_pagina='facebook');
+
+select data_nasc from Pessoas where mail in ( select mail from Contem_Registos natural join Editam where nome_pagina = 'facebook' and mail in (
+	select mail from Contem_Registos natural join Editam where nome_registo = 'facebook'));
