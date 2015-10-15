473935b165342f2d0ec4183abfae4ca85d08c35f
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2015-10-15 16:12:41 +0100

    queries para teste - incomplete

---
+++ queries.sql
@@ -0,0 +1,30 @@
+insert into Pessoas values ('gffsac@hotmail.com', 'Gabriel', '1234');
+insert into Pessoas values ('fabricio@hotmail.com', 'Fabricio', '1234');
+insert into Pessoas values ('comba@mal.com', 'Matue', '1234');
+insert into Pessoas values ('afinfa@naco.com', 'Jose', '1234');
+
+insert into Paginas values ('As minhas contas');
+insert into Paginas values ('aulas no tecnico');
+insert into Paginas values ('notas provas aval');
+insert into Paginas values ('amigos e companhia');
+insert into Paginas values ('a minha horta');
+
+insert into Tipos_Registo values ('contas');
+insert into Tipos_Registo values ('aulas');
+insert into Tipos_Registo values ('notas');
+insert into Tipos_Registo values ('amigos');
+insert into Tipos_Registo values ('galinhas');
+
+insert into Campos values ('divida', 'contas');
+insert into Campos values ('lucro', 'contas');
+insert into Campos values ('balance', 'contas');
+insert into Campos values ('disciplina', 'aulas');
+insert into Campos values ('prof', 'aulas');
+insert into Campos values ('trabalho', 'aulas');
+insert into Campos values ('idade', 'galinhas');
+insert into Campos values ('tipo', 'galinhas');
+insert into Campos values ('tropas', 'amigos');
+insert into Campos values ('colegas', 'amigos');
+insert into Campos values ('falsos', 'amigos');
+insert into Campos values ('positiva', 'notas');
+insert into Campos values ('negativa', 'notas');
